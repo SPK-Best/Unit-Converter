26c2c9b93ec8db2367ff805392d845a1902e2e09
SPK-Best <65452528+SPK-Best@users.noreply.github.com>
2023-01-11 20:02:47 +0700

    Initial Commit

---
+++ Unit-Converter/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Unit-Converter
+//
+//  Created by Supakrit Nutchanart on 11/1/2566 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Unit-Converter/Unit_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unit_ConverterApp.swift
+//  Unit-Converter
+//
+//  Created by Supakrit Nutchanart on 11/1/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct Unit_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
