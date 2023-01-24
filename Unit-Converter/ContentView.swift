6eb0e907a59dca6cac4d4f40d5412c4a5f4a1c93
SPK-Best <65452528+SPK-Best@users.noreply.github.com>
2023-01-24 18:52:08 +0700

    Converter Logic and Result Text

--- Unit-Converter/ContentView.swift
+++ Unit-Converter/ContentView.swift
@@ -9,33 +9,58 @@ import SwiftUI
 
 struct ContentView: View {
     @State var inputNumber: String = "";
+    @State var resultNumber: String = "";
     var body: some View {
         VStack {
             VStack {
                 VStack {
-                    Text("Unit Converter")
-                        .font(.title)
-                        .foregroundColor(.blue)
-                        .fontWeight(.bold)
-                }
-                .padding()
-                TextField("Input Number", text: $inputNumber)
+                    VStack {
+                        Text("Unit Converter")
+                            .font(.title)
+                            .foregroundColor(.black)
+                            .fontWeight(.bold)
+                            .padding(.bottom)
+                        Text("Convert number from KG to IBS.")
+                            .font(.headline)
+                            .foregroundColor(.black)
+                            .fontWeight(.bold)
+                            
+                    }.padding()
+                    TextField("Input Number", text: $inputNumber)
+                        .padding(.all)
+                        .border(.blue)
+                        .keyboardType(.numberPad)
+                }.padding()
+                HStack {
+                    Button("Reset") {
+                        self.inputNumber = "";
+                        self.resultNumber = "";
+                    }
+                    .font(.title3)
                     .padding(.all)
-                    .border(.blue)
-            }.padding()
-            HStack {
-                Button("Reset") {
-                    self.inputNumber = "";
+                    .border(.red)
+                    .foregroundColor(.blue)
+                    Button("Convert") {
+                        let inputInDouble = Double(inputNumber);
+                        var result = (inputInDouble ?? 0.0) * Double(2.2)
+                        result = result.rounded(.up)
+                        self.resultNumber = String(result) + " pounds";
+                    }
+                    .font(.title3)
+                    .padding(.all)
+                    .border(.green)
+                    .backgroundStyle(.pink)
                 }
+            }.padding(.bottom)
+            Text("Result")
+                .font(.title)
+                .fontWeight(.bold)
                 .padding(.all)
-                .border(.red)
+            Text(resultNumber)
                 .foregroundColor(.blue)
-                Button("Convert") {
-                }                
+                .font(.title2)
+                .fontWeight(.bold)
                 .padding(.all)
-                .border(.green)
-                .backgroundStyle(.pink)
-            }
         }
     }
 }
